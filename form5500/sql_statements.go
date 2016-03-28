c55cc7c814d1de76b97b9ccd900b466374fc295c
John Calvin <calvjd@gmail.com>
2016-03-28 10:45:18 -0700

    fix mat view

--- form5500/sql_statements.go
+++ form5500/sql_statements.go
@@ -22,7 +22,7 @@ func createMaterializedView() []string {
 		`CREATE MATERIALIZED VIEW form5500_search_view AS
   		SELECT *, to_tsvector(sponsor_name) || to_tsvector(sponsor_ein)
    			as sponsor_search
-  		FROM (SELECT DISTINCT ON (sponsor_name, sponsor_ein) * FROM form_5500_search ORDER BY sponsor_name, sponsor_ein, date_received DESC) as foo;`,
+  		FROM (SELECT DISTINCT ON (plan_num, sponsor_ein) * FROM form_5500_search ORDER BY plan_num, sponsor_ein, date_received DESC) as foo;`,
 		"CREATE INDEX idx_fts_sponsor ON form5500_search_view USING gin(sponsor_search);",
 	}
 	return executableStatements

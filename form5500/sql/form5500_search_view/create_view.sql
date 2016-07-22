27b75b2b7a62a1f158999721d348a7dab6106613
John Calvin <calvjd@gmail.com>
2016-07-21 17:43:57 -0700

    fix typo in sql file

--- form5500/sql/form5500_search_view/create_view.sql
+++ form5500/sql/form5500_search_view/create_view.sql
@@ -2,6 +2,6 @@ DROP MATERIALIZED VIEW IF EXISTS form5500_search_view;
 CREATE MATERIALIZED VIEW form5500_search_view AS
   (SELECT *, to_tsvector(sponsor_name) || to_tsvector(sponsor_ein)
     as sponsor_search
-  FROM (SELECT DISTINCT ON (plan_num, sponsor_ein) * FROM form_5500_search ORDER BY plan_num, sponsor_ein, date_received DESC) as foo;)
+  FROM (SELECT DISTINCT ON (plan_num, sponsor_ein) * FROM form_5500_search ORDER BY plan_num, sponsor_ein, date_received DESC) as foo);
 
 CREATE INDEX idx_fts_sponsor ON form5500_search_view USING gin(sponsor_search);

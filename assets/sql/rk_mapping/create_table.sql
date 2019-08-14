a1d14ce164650c3d0884bdb0088aba7146e58f66
Liz Gabriel <lgabriel@fiduciarybenchmarks.com>
2019-08-14 13:38:06 -0700

    fbd-472 - put primary key on name field

--- assets/sql/rk_mapping/create_table.sql
+++ assets/sql/rk_mapping/create_table.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS sched_c_provider_to_fbi_rk_company_id_mappings;
-CREATE TABLE sched_c_provider_to_fbi_rk_company_id_mappings ( name varchar(100) NOT NULL, fbi_id INTEGER NOT NULL);
+CREATE TABLE sched_c_provider_to_fbi_rk_company_id_mappings ( name varchar(100) PRIMARY KEY, fbi_id INTEGER NOT NULL);
+
